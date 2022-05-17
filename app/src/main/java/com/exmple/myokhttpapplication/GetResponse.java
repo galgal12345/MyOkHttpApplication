ced872aab62b21b241483e7c0ec7d70bf460894e
GilAlmuly <almuly.gil212@gmail.com>
2022-05-17 07:27:01 +0300

    separated Presenter class to 2 class class that gets response and presenter use it

---
+++ app/src/main/java/com/exmple/myokhttpapplication/GetResponse.java
@@ -0,0 +1,40 @@
+package com.exmple.myokhttpapplication;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class GetResponse {
+
+    public static void onMyGetResponse(String url) {
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String myResponse = response.body().string();
+                    Log.v("TAG", "my response.body() is: " + myResponse);
+
+                }
+            }
+        });
+    }
+
+
+}
--- app/src/main/java/com/exmple/myokhttpapplication/Presenter.java
+++ app/src/main/java/com/exmple/myokhttpapplication/Presenter.java
@@ -1,49 +1,19 @@
 package com.exmple.myokhttpapplication;
 
-import android.util.Log;
-
-import androidx.annotation.NonNull;
-
-import java.io.IOException;
-
-import okhttp3.Call;
-import okhttp3.Callback;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 public class Presenter implements AppView {
 
-    OkHttpClient client = new OkHttpClient();
-
     @Override
     public void getMovieDbResult() {
-        onMyGetResponse("https://api.themoviedb.org/3/movie/popular?api_key=1a6249896845e0d12ad0e1a3acc66c5c");
+        GetResponse.onMyGetResponse("https://api.themoviedb.org/3/movie/popular?api_key=1a6249896845e0d12ad0e1a3acc66c5c");
     }
 
     @Override
     public void getDogApiResult() {
-        onMyGetResponse("https://api.thedogapi.com/v1/images/search");
+        GetResponse.onMyGetResponse("https://api.thedogapi.com/v1/images/search");
     }
 
-    public void onMyGetResponse(String url) {
-        Request request = new Request.Builder().url(url).build();
 
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                e.printStackTrace();
-            }
-
-            @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                if (response.isSuccessful()) {
-                    String myResponse = response.body().string();
-                    Log.v("TAG", "my response.body() is: " + myResponse);
-
-                }
-            }
-        });
-    }
 
 }
